5166e5dd64ed336b9cf600fbeca2320792751a88
development <development@gamtgroup.com>
2026-01-20 10:51:46 -0500

    build:estrucutra base

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import ejs from "ejs";
+import routes from "./routes.js";
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(express.static("public"));
+app.use("/", routes);
+
+app.listen(3050, () => {
+    console.log("Server started on port http://localhost:3050");
+});
